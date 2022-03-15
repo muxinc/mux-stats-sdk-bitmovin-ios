f777a3c15fcef820fa6d3393b735f1cbdfac7a04
Stephanie Zuniga <szuniga@konradgroup.com>
2022-03-14 18:40:43 -0600

    Initial pod setup

--- update-release-xcframeworks-static.sh
+++ update-release-xcframeworks-static.sh
@@ -1,5 +1,5 @@
-BUILD_DIR=$PWD/MUXSDKKaltura/xc
-PROJECT=$PWD/MUXSDKKaltura/MUXSDKKaltura.xcworkspace
+BUILD_DIR=$PWD/MUXSDKBitmovin/xc
+PROJECT=$PWD/MUXSDKBitmovin/MUXSDKBitmovin.xcworkspace
 TARGET_DIR=$PWD/XCFramework
 
 
@@ -13,23 +13,19 @@ mkdir -p $TARGET_DIR
 
 ################ Build MuxCore SDK                                                                                                                                                             
 
-xcodebuild archive -scheme MUXSDKKalturaTv -workspace $PROJECT -destination "generic/platform=tvOS" -archivePath "$BUILD_DIR/MUXSDKKalturaTv.tvOS.xcarchive" SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DIS\
+xcodebuild archive -scheme MUXSDKBitmovinTv -workspace $PROJECT -destination "generic/platform=tvOS" -archivePath "$BUILD_DIR/MUXSDKBitmovinTv.tvOS.xcarchive" SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DIS\
 TRIBUTION=YES CLANG_ENABLE_MODULES=NO MACH_O_TYPE=staticlib
- xcodebuild archive -scheme MUXSDKKalturaTv -workspace $PROJECT -destination "generic/platform=tvOS Simulator" -archivePath "$BUILD_DIR/MUXSDKKalturaTv.tvOS-simulator.xcarchive" SKIP_INSTALL=NO \
+ xcodebuild archive -scheme MUXSDKBitmovinTv -workspace $PROJECT -destination "generic/platform=tvOS Simulator" -archivePath "$BUILD_DIR/MUXSDKBitmovinTv.tvOS-simulator.xcarchive" SKIP_INSTALL=NO \
 BUILD_LIBRARY_FOR_DISTRIBUTION=YES CLANG_ENABLE_MODULES=NO MACH_O_TYPE=staticlib
- xcodebuild archive -scheme MUXSDKKaltura -workspace $PROJECT  -destination "generic/platform=iOS" -archivePath "$BUILD_DIR/MUXSDKKaltura.iOS.xcarchive" SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIB\
+ xcodebuild archive -scheme MUXSDKBitmovin -workspace $PROJECT  -destination "generic/platform=iOS" -archivePath "$BUILD_DIR/MUXSDKBitmovin.iOS.xcarchive" SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIB\
 UTION=YES CLANG_ENABLE_MODULES=NO MACH_O_TYPE=staticlib
- xcodebuild archive -scheme MUXSDKKaltura -workspace $PROJECT  -destination "generic/platform=iOS Simulator" -archivePath "$BUILD_DIR/MUXSDKKaltura.iOS-simulator.xcarchive" SKIP_INSTALL=NO BUILD\
+ xcodebuild archive -scheme MUXSDKBitmovin -workspace $PROJECT  -destination "generic/platform=iOS Simulator" -archivePath "$BUILD_DIR/MUXSDKBitmovin.iOS-simulator.xcarchive" SKIP_INSTALL=NO BUILD\
 _LIBRARY_FOR_DISTRIBUTION=YES CLANG_ENABLE_MODULES=NO MACH_O_TYPE=staticlib
 
-  xcodebuild archive -scheme MUXSDKKaltura -workspace $PROJECT  -destination "generic/platform=macOS,variant=Mac Catalyst" -archivePath "$BUILD_DIR/MUXSDKKaltura.macOS.xcarchive" SKIP_INSTALL=NO\
- BUILD_LIBRARY_FOR_DISTRIBUTION=YES CLANG_ENABLE_MODULES=NO MACH_O_TYPE=staticlib
-
- xcodebuild -create-xcframework -framework "$BUILD_DIR/MUXSDKKalturaTv.tvOS.xcarchive/Products/Library/Frameworks/MUXSDKKaltura.framework" \
-                                -framework "$BUILD_DIR/MUXSDKKalturaTv.tvOS-simulator.xcarchive/Products/Library/Frameworks/MUXSDKKaltura.framework" \
-                                -framework "$BUILD_DIR/MUXSDKKaltura.iOS.xcarchive/Products/Library/Frameworks/MUXSDKKaltura.framework" \
-                                -framework "$BUILD_DIR/MUXSDKKaltura.iOS-simulator.xcarchive/Products/Library/Frameworks/MUXSDKKaltura.framework" \
-                                -framework "$BUILD_DIR/MUXSDKKaltura.macOS.xcarchive/Products/Library/Frameworks/MUXSDKKaltura.framework" \
-                                -output "$TARGET_DIR/MUXSDKKaltura.xcframework"
+ xcodebuild -create-xcframework -framework "$BUILD_DIR/MUXSDKBitmovinTv.tvOS.xcarchive/Products/Library/Frameworks/MUXSDKBitmovin.framework" \
+                                -framework "$BUILD_DIR/MUXSDKBitmovinTv.tvOS-simulator.xcarchive/Products/Library/Frameworks/MUXSDKBitmovin.framework" \
+                                -framework "$BUILD_DIR/MUXSDKBitmovin.iOS.xcarchive/Products/Library/Frameworks/MUXSDKBitmovin.framework" \
+                                -framework "$BUILD_DIR/MUXSDKBitmovin.iOS-simulator.xcarchive/Products/Library/Frameworks/MUXSDKBitmovin.framework" \
+                                -output "$TARGET_DIR/MUXSDKBitmovin.xcframework"
 
 rm -Rf $BUILD_DIR
