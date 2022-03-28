eb2fad46678cb42150069eda51862758cb7c79e7
StephanieZuniga <40036667+StephanieZuniga@users.noreply.github.com>
2022-03-28 15:23:43 -0600

    Example apps
    * Add DemoApp-swift
    * Add tvOS target
    * Add objective c app

---
+++ apps/DemoApp_objc/DemoApp_objc/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  DemoApp_objc
+//
+//  Created by Stephanie Zuñiga on 17/3/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
